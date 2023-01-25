b0b6d67b9e6041bbad40ea1fd7c878549eb9e4dc
adamx <adamxpankowski@gmail.com>
2023-01-25 09:08:18 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserDao;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserDao, Integer>
+{
+ UserDao findByUsername(String username);
+} 
